c0782c7ad482c9a02e31057309bb832faa4166d0
smolck <46855713+smolck@users.noreply.github.com>
2020-05-11 22:53:10 -0500

    Fix settings page overflow issue/refine settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -15,64 +15,84 @@ class SettingsPage extends StatelessWidget {
         padding: EdgeInsets.only(left: 24.0),
         child: Column(
           children: <Widget>[
-            Consumer<PageModel>(
-              builder: (context, pageModel, _) {
-                return GestureDetector(
-                  onTap: () => pageModel.setCurrentPage(AppPage.Info),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Icon(
-                          OMIcons.info,
-                          size: 40.0,
-                          color: palette['red'],
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Consumer<PageModel>(
+                builder: (context, pageModel, _) {
+                  return GestureDetector(
+                    onTap: () => pageModel.setCurrentPage(AppPage.Info),
+                    child: RichText(
+                      softWrap: false,
+                      text: TextSpan(
+                        children: [
+                          WidgetSpan(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 12.0, right: 12.0),
+                              child: Icon(
+                                OMIcons.info,
+                                size: 30.0,
+                                color: palette['red'],
+                              ),
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'What exactly is RWSSP?',
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 22.0,
+                              color: palette['red'],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      WidgetSpan(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 12.0, right: 12.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(40.0),
+                                bottomLeft: Radius.circular(40.0),
+                              ),
+                              color: palette['red'].withAlpha(30),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Icon(
+                                Icons.favorite,
+                                color: palette['red'],
+                                size: 15.0,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      Text(
-                        'What exactly is R-W-S-S-P?',
+                      TextSpan(
+                        text: 'Favorites',
                         style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 24.0,
-                          color: palette['red'],
-                        ),
+                          textBaseline: TextBaseline.alphabetic,
+                            fontFamily: 'Montserrat',
+                            fontSize: 22.0,
+                            color: palette['grey']), // TODO: Use 7F7F7F?
                       ),
+                      WidgetSpan(child: Spacer()),
                     ],
                   ),
-                );
-              },
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40.0),
-                        bottomLeft: Radius.circular(40.0),
-                      ),
-                      color: palette['red'].withAlpha(30),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Icon(
-                        Icons.favorite,
-                        color: palette['red'],
-                        size: 15.0,
-                      ),
-                    ),
-                  ),
                 ),
-                Text(
-                  'Favorites',
-                  style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 24.0,
-                      color: palette['grey']), // TODO: Use 7F7F7F?
-                )
-              ],
-            )
+              ),
+            ),
           ],
         ),
       ),
