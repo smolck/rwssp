c6d5df97d577cf8549c48ac37001aa9b3a202825
smolck <46855713+smolck@users.noreply.github.com>
2020-05-17 22:26:43 -0500

    Clean/improve up settings page (#2)

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -6,94 +6,107 @@ import '../models/page_model.dart';
 import '../styles.dart';
 
 class SettingsPage extends StatelessWidget {
+  Center _bottomButton(
+    BuildContext context, {
+    @required void Function() onPressed,
+    @required String label,
+  }) =>
+      Center(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: InkWell(
+            onTap: onPressed,
+            child: Text(
+              label,
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 14.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Padding _button(
+    BuildContext context, {
+    @required void Function() onPressed,
+    @required String label,
+    @required IconData iconData,
+  }) =>
+      Padding(
+        padding: const EdgeInsets.only(left: 49.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: FlatButton.icon(
+            onPressed: onPressed,
+            label: Text(
+              label,
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 16.0,
+                color: palette['black'],
+              ),
+            ),
+            icon: Icon(
+              iconData,
+              size: 34.0,
+              color: palette['black'],
+            ),
+          ),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      // TODO(smolck): Fix padding?
-      child: Padding(
-        padding: EdgeInsets.only(left: 24.0),
-        child: Column(
-          children: <Widget>[
-            Align(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 49.0),
+            child: Align(
               alignment: Alignment.centerLeft,
               child: Consumer<PageModel>(
                 builder: (context, pageModel, _) {
-                  return GestureDetector(
-                    onTap: () => pageModel.setCurrentPage(AppPage.Info),
-                    child: RichText(
-                      softWrap: false,
-                      text: TextSpan(
-                        children: [
-                          WidgetSpan(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 12.0, right: 12.0),
-                              child: Icon(
-                                OMIcons.info,
-                                size: 30.0,
-                                color: palette['red'],
-                              ),
-                            ),
-                          ),
-                          TextSpan(
-                            text: 'What exactly is RWSSP?',
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 22.0,
-                              color: palette['red'],
-                            ),
-                          ),
-                        ],
+                  return FlatButton.icon(
+                    label: Text(
+                      'What Exactly is RWSSP?',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        color: palette['black'],
                       ),
                     ),
+                    icon: Icon(
+                      OMIcons.info,
+                      size: 34.0,
+                      color: palette['black'],
+                    ),
+                    color: Colors.transparent,
+                    onPressed: () => pageModel.setCurrentPage(AppPage.Info),
                   );
                 },
               ),
             ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      WidgetSpan(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 12.0, right: 12.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(40.0),
-                                bottomLeft: Radius.circular(40.0),
-                              ),
-                              color: palette['red'].withAlpha(30),
-                            ),
-                            child: Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: Icon(
-                                Icons.favorite,
-                                color: palette['red'],
-                                size: 15.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Favorites',
-                        style: TextStyle(
-                          textBaseline: TextBaseline.alphabetic,
-                            fontFamily: 'Montserrat',
-                            fontSize: 22.0,
-                            color: palette['grey']), // TODO(smolck): Use 7F7F7F?
-                      ),
-                      WidgetSpan(child: Spacer()),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+
+          // TODO(smolck): Add functionality for each of these buttons.
+          _button(context,
+              onPressed: () {}, iconData: OMIcons.book, label: 'Versions'),
+          _button(context,
+              onPressed: () {},
+              iconData: OMIcons.edit,
+              label: 'Customize Memorization'),
+          _button(context,
+              onPressed: () {}, iconData: OMIcons.history, label: 'History'),
+          _button(context,
+              onPressed: () {},
+              iconData: OMIcons.brightnessMedium,
+              label: 'Theme'),
+          Spacer(),
+          _bottomButton(context, onPressed: () {}, label: 'About this app'),
+          _bottomButton(context, onPressed: () {}, label: 'Feedback'),
+        ],
       ),
     );
   }
